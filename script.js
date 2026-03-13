bfd1c37e6258c616e7587c104576276de4707062
Dmitry <maiiamphiphi@gmail.com>
2026-03-13 06:38:22 +0000

    write getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello, World!');
+function getComputerChoice() {
+    let optionsArr = ['rock', 'paper', 'scissors'];
+    let indexOptions = Math.floor(Math.random() * 3);
+
+    return optionsArr[indexOptions];
+}
+
