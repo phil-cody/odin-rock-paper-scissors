c1da0bee9a3a2c49a9dc68e3da5ab9116a064086
Dmitry <maiiamphiphi@gmail.com>
2026-03-19 19:13:27 +0700

    add title, subtitle and restart button

--- script.js
+++ script.js
@@ -1,91 +1,114 @@
 let humanScore = 0;
 let computerScore = 0;
+let roundNumber = 1;
 
-let choiceArr = document.querySelector('.choice');
-let resultMessage = document.querySelector('.result');
+let choiceArr = document.querySelector(".choice");
+let resultMessage = document.querySelector(".result");
+let roundNumberContent = document.querySelector(".result__round-number");
+let resultScore = document.querySelector(".result__score");
+let startOver = document.querySelector("#restart__button");
+console.log(startOver);
 
-const resultChoice = document.createElement('p');
-const resultRound = document.createElement('p');
-const resultScore = document.createElement('p');
+const resultChoice = document.createElement("p");
+const resultRound = document.createElement("p");
 
-const wonMessage = document.createElement('p');
-const loseMessage = document.createElement('p');
+const wonMessage = document.createElement("p");
+const loseMessage = document.createElement("p");
 
-wonMessage.textContent = 'You won! Congratulations!';
-loseMessage.textContent = 'Oh, maybe you`ll be lucky next time :)';
+wonMessage.textContent = "You won! Congratulations!";
+loseMessage.textContent = "Oh, maybe you`ll be lucky next time :)";
 
 function getComputerChoice() {
-    let optionsArr = ['Rock', 'Paper', 'Scissors'];
-    let indexOptions = Math.floor(Math.random() * 3);
+  let optionsArr = ["Rock", "Paper", "Scissors"];
+  let indexOptions = Math.floor(Math.random() * 3);
 
-    return optionsArr[indexOptions];
+  return optionsArr[indexOptions];
 }
 
 function playRound(humanChoice, computerChoice) {
-    let optionsArr = ['Rock', 'Paper', 'Scissors'];
-    let resultArr = ['Draw! You think the same', 'You lose!', 'You won!'];
+  let optionsArr = ["Rock", "Paper", "Scissors"];
+  let resultArr = ["Draw! You think the same", "You lose!", "You won!"];
 
-    const humanRock = humanChoice === optionsArr[0];
-    const humanPaper = humanChoice === optionsArr[1];
-    const humanScissors = humanChoice === optionsArr[2];
+  const humanRock = humanChoice === optionsArr[0];
+  const humanPaper = humanChoice === optionsArr[1];
+  const humanScissors = humanChoice === optionsArr[2];
 
-    const computerRock = computerChoice === optionsArr[0];
-    const computerPaper = computerChoice === optionsArr[1];
-    const computerScissors = computerChoice === optionsArr[2];
+  const computerRock = computerChoice === optionsArr[0];
+  const computerPaper = computerChoice === optionsArr[1];
+  const computerScissors = computerChoice === optionsArr[2];
 
-    if (humanChoice === computerChoice) {
-        resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
-        resultRound.textContent = `${resultArr[0]}`;
-        resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
-    } else if (humanRock && computerPaper) {
-        computerScore++;
-        resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
-        resultRound.textContent = `${resultArr[1]}`;
-        resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
-    } else if (humanRock && computerScissors) {
-        humanScore++;
-        resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
-        resultRound.textContent = `${resultArr[2]}`;
-        resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
-    } else if (humanPaper && computerRock) {
-        humanScore++;
-        resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
-        resultRound.textContent = `${resultArr[2]}`;
-        resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
-    } else if (humanPaper && computerScissors) {
-        computerScore++;
-        resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
-        resultRound.textContent = `${resultArr[1]}`;
-        resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
-    } else if (humanScissors && computerRock) {
-        computerScore++;
-        resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
-        resultRound.textContent = `${resultArr[1]}`;
-        resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
-    } else if (humanScissors && computerPaper) {
-        humanScore++;
-        resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
-        resultRound.textContent = `${resultArr[2]}`;
-        resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
-    }
+  if (humanChoice === computerChoice) {
+    roundNumberContent.textContent = `Round ${roundNumber}`;
+    resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
+    resultRound.textContent = `${resultArr[0]}`;
+    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
+  } else if (humanRock && computerPaper) {
+    computerScore++;
+    roundNumberContent.textContent = `Round ${roundNumber}`;
+    resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
+    resultRound.textContent = `${resultArr[1]}`;
+    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
+  } else if (humanRock && computerScissors) {
+    humanScore++;
+    roundNumberContent.textContent = `Round ${roundNumber}`;
+    resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
+    resultRound.textContent = `${resultArr[2]}`;
+    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
+  } else if (humanPaper && computerRock) {
+    humanScore++;
+    roundNumberContent.textContent = `Round ${roundNumber}`;
+    resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
+    resultRound.textContent = `${resultArr[2]}`;
+    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
+  } else if (humanPaper && computerScissors) {
+    computerScore++;
+    roundNumberContent.textContent = `Round ${roundNumber}`;
+    resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
+    resultRound.textContent = `${resultArr[1]}`;
+    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
+  } else if (humanScissors && computerRock) {
+    computerScore++;
+    roundNumberContent.textContent = `Round ${roundNumber}`;
+    resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
+    resultRound.textContent = `${resultArr[1]}`;
+    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
+  } else if (humanScissors && computerPaper) {
+    humanScore++;
+    roundNumberContent.textContent = `Round ${roundNumber}`;
+    resultChoice.textContent = `You : ${humanChoice} | ${computerChoice} : Computer`;
+    resultRound.textContent = `${resultArr[2]}`;
+    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
+  }
+
+  roundNumber++;
 }
 
-choiceArr.addEventListener('click', function playGame(event) {
-    let target = event.target;
-    
-    let playerChoice = target.textContent;
+choiceArr.addEventListener("click", function playGame(event) {
+  let target = event.target;
 
-    playRound(playerChoice, getComputerChoice());
+  let playerChoice = target.id.charAt(0).toUpperCase() + target.id.slice(1);
 
-    resultMessage.appendChild(resultChoice);
-    resultMessage.appendChild(resultRound);
-    resultMessage.appendChild(resultScore);
+  playRound(playerChoice, getComputerChoice());
 
-    if (humanScore >= 5) {
-        choiceArr.removeEventListener('click', playGame);
-        resultMessage.appendChild(wonMessage);
-    } else if (computerScore >= 5) {
-        choiceArr.removeEventListener('click', playGame);
-        resultMessage.appendChild(loseMessage);
-    }
-})
+  resultMessage.appendChild(resultChoice);
+  resultMessage.appendChild(resultRound);
+  resultMessage.appendChild(resultScore);
+
+  if (humanScore >= 5) {
+    choiceArr.removeEventListener("click", playGame);
+    resultMessage.appendChild(wonMessage);
+  } else if (computerScore >= 5) {
+    choiceArr.removeEventListener("click", playGame);
+    resultMessage.appendChild(loseMessage);
+  }
+});
+
+startOver.addEventListener("click", () => {
+    humanScore = 0;
+    computerScore = 0;
+    roundNumber = 1;
+    roundNumberContent.textContent = `Are you sure you're ready to repeat it?`;
+    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
+    resultChoice.remove();
+    resultRound.remove();
+});
