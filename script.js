71ea4f23a4ad5dcdd5754f05e9c33245d640beb0
Dmitry <maiiamphiphi@gmail.com>
2026-03-29 13:15:55 +0700

    add style for this game

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ let humanScore = 0;
 let computerScore = 0;
 let roundNumber = 1;
 
-let choiceArr = document.querySelector(".choice");
-let resultMessage = document.querySelector(".result");
-let roundNumberContent = document.querySelector(".result__round-number");
-let resultScore = document.querySelector(".result__score");
-let startOver = document.querySelector("#restart__button");
-console.log(startOver);
+const choiceArr = document.querySelector(".choice");
+const resultMessage = document.querySelector(".result");
+const roundNumberContent = document.querySelector(".result__round-number");
+const startOver = document.querySelector("#restart__button");
 
+const resultScore = document.createElement("p");
 const resultChoice = document.createElement("p");
 const resultRound = document.createElement("p");
 
@@ -16,7 +15,10 @@ const wonMessage = document.createElement("p");
 const loseMessage = document.createElement("p");
 
 wonMessage.textContent = "You won! Congratulations!";
+wonMessage.classList.add('result__winner');
+
 loseMessage.textContent = "Oh, maybe you`ll be lucky next time :)";
+loseMessage.classList.add('result__loser');
 
 function getComputerChoice() {
   let optionsArr = ["Rock", "Paper", "Scissors"];
@@ -83,12 +85,26 @@ function playRound(humanChoice, computerChoice) {
   roundNumber++;
 }
 
-choiceArr.addEventListener("click", function playGame(event) {
+function playGame(event) {
   let target = event.target;
 
   let playerChoice = target.id.charAt(0).toUpperCase() + target.id.slice(1);
 
   playRound(playerChoice, getComputerChoice());
+
+  if (resultRound.textContent === 'You won!' && !resultRound.classList.contains('result__winner')) {
+    resultRound.classList.remove('result__loser');
+    resultRound.classList.remove('result__default');
+    resultRound.classList.add('result__winner');
+  } else if (resultRound.textContent === 'You lose!' && !resultRound.classList.contains('result__loser')) {
+    resultRound.classList.remove('result__winner');
+    resultRound.classList.remove('result__default');
+    resultRound.classList.add('result__loser');
+  } else  if (resultRound.textContent === 'Draw! You think the same' && !resultRound.classList.contains('result__default')) {
+    resultRound.classList.remove('result__winner');
+    resultRound.classList.remove('result__loser');
+    resultRound.classList.add('result__default');
+  }
 
   resultMessage.appendChild(resultChoice);
   resultMessage.appendChild(resultRound);
@@ -101,14 +117,26 @@ choiceArr.addEventListener("click", function playGame(event) {
     choiceArr.removeEventListener("click", playGame);
     resultMessage.appendChild(loseMessage);
   }
-});
+}
+
+choiceArr.addEventListener("click", playGame);
 
 startOver.addEventListener("click", () => {
-    humanScore = 0;
-    computerScore = 0;
-    roundNumber = 1;
-    roundNumberContent.textContent = `Are you sure you're ready to repeat it?`;
-    resultScore.textContent = `Player : ${humanScore} | ${computerScore} : Computer`;
-    resultChoice.remove();
-    resultRound.remove();
+  humanScore = 0;
+  computerScore = 0;
+  roundNumber = 1;
+
+  roundNumberContent.textContent = `Are you sure you're ready to repeat it?`;
+  resultScore.remove();
+  resultChoice.remove();
+  resultRound.remove();
+
+  if (
+    resultMessage.lastElementChild === wonMessage ||
+    resultMessage.lastElementChild === loseMessage
+  ) {
+    resultMessage.lastElementChild.remove();
+  }
+
+  choiceArr.addEventListener("click", playGame);
 });
